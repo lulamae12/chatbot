41e01949a2ea648af77eb4f41d263b2ec32cd6d2
Tommy Smith <tls.tommy11@gmail.com>
2018-03-02 11:02:04 -0600

    made succesful read progress

--- CornellChatbot/CornellChatbot.py
+++ CornellChatbot/CornellChatbot.py
@@ -12,3 +12,32 @@ conv_lines = open('movie_conversations.txt', encoding='utf-8', errors='ignore').
 #sentences to train
 lines[:10]
 conv_lines[:10]
+
+#dict to map line id with text
+id2line = {}
+for line in lines:
+    _line = line.split(' +++$+++ ')
+    if len(_line) == 5:
+        id2line[_line[0]] = _line[4]
+#list of converstions line ids
+convs = [ ]
+for line in conv_lines[:-1]:
+    _line = line.split(' +++$+++ ')[-1][1:-1].replace("'","").replace(" ","")
+    convs.append(_line.split(','))
+convs[:10]
+
+#sort into questions(input) or answers (target/output)
+questions = []
+answers = []
+
+for conv in convs:
+    for i in range(len(conv)-1):
+        questions.append(id2line[conv[i]])
+        answers.append(id2line[conv[i+1]])
+
+#debugtest
+limit = 0
+for i in range(limit, limit+5):
+    print(questions[i])
+    print(answers[i])
+    print()
