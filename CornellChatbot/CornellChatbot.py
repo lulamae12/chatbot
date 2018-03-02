5dd7d6e93fb2726e7060e69f3f201eff9c22ae52
Tommy Smith <tls.tommy11@gmail.com>
2018-03-02 11:07:05 -0600

    cleanup

--- CornellChatbot/CornellChatbot.py
+++ CornellChatbot/CornellChatbot.py
@@ -45,3 +45,31 @@ for i in range(limit, limit+5):
 #compare question/answer langth
 print(f"Question Length:",len(questions))
 print(f"Answer Length:",len(answers))
+
+def textcleanup(text):
+#cleansup text by removing unnecessary characters like is and reformats.
+
+    text = text.lower()
+
+    text = re.sub(r"i'm", "i am", text)
+    text = re.sub(r"he's", "he is", text)
+    text = re.sub(r"she's", "she is", text)
+    text = re.sub(r"it's", "it is", text)
+    text = re.sub(r"that's", "that is", text)
+    text = re.sub(r"what's", "that is", text)
+    text = re.sub(r"where's", "where is", text)
+    text = re.sub(r"how's", "how is", text)
+    text = re.sub(r"\'ll", " will", text)
+    text = re.sub(r"\'ve", " have", text)
+    text = re.sub(r"\'re", " are", text)
+    text = re.sub(r"\'d", " would", text)
+    text = re.sub(r"\'re", " are", text)
+    text = re.sub(r"won't", "will not", text)
+    text = re.sub(r"can't", "cannot", text)
+    text = re.sub(r"n't", " not", text)
+    text = re.sub(r"n'", "ng", text)
+    text = re.sub(r"'bout", "about", text)
+    text = re.sub(r"'til", "until", text)
+    text = re.sub(r"[-()\"#/@;:<>{}`+=~|.!?,]", "", text)
+
+    return text
